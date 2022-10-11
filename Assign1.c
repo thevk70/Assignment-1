0d008f6fae9fc46eefb47c7dad87a561b7906e03
Vishwajeet Kumar <100605183+thevk70@users.noreply.github.com>
2022-10-11 10:08:30 +0530

    Add files via upload

---
+++ Assign1.c
@@ -0,0 +1,123 @@
+//Q1. Write a program to print Hello Students on the screen.
+// #include <stdio.h>
+// int main()
+// {
+//     printf("Hello Students");
+//     return 0;
+// }
+
+//Q2. write a program to print Hello in the first line and Students in the second line.
+// #include <stdio.h>
+// int main()
+// {
+//     printf("Hello");
+//     printf("\nStudents");
+//     return 0;
+// }
+
+//Q3. Write a program to print "iNeuron" on the screen. (Remember to print in double quotes).
+// #include <stdio.h>
+// int main()
+// {
+//     printf("\"iNeuron\"");
+//     return 0;
+// }
+
+//Q4.  WAP to find the area of the circle. Take radius of circle from user as input and print the result in below given format.
+//     Expected output format – “Area of circle is A having the radius R”. Replace A with area & R with radius.
+// #include <stdio.h>
+// int main()
+// {
+//     int r,area;
+//     printf("Enter the value of radius :: ");
+//     scanf("%d",&r);
+//     area = 3.14 * (r*r);
+//     printf("Area of circle is %d having the radius %d",area,r);
+//     return 0;
+// }
+
+//Q5. WAP to calculate the length of String using printf function.
+// #include <stdio.h>
+// int main()
+// {
+//     char str[] = "Vishu";
+//     int a = sizeof(str);
+//     printf("%d",a);
+//     return 0;
+// }
+
+//Q6. WAP to print the name of the user in double quotes.
+//   Expected output format – “Hello , Amit Kumar”
+// #include <stdio.h>
+// int main()
+// {
+//     printf("\"Hello , Amit Kumar\"");
+//     return 0;
+// }
+
+//Q7. WAP to print “%d” on the screen.
+//  #include <stdio.h>
+//  int main()
+//  {
+//      printf("\"%%d\"");
+//      return 0;
+//  }
+
+//Q8. WAP to print “\n” on the screen.
+// #include <stdio.h>
+// int main()
+// {
+//     printf("\"\\n\"");
+//     return 0;
+// }
+
+//Q9.WAP to print “\\” on the screen.
+// #include <stdio.h>
+// int main()
+// {
+//     printf("\"\\\\\"");
+//     return 0;
+// }
+
+//Q10.  WAP to take date as an input in below given format and convert the date format and
+// display the result as given below.
+// User Input date format – “DD/MM/YYYY” (27/11/2022)
+// Output format –
+// “Day – DD , Month – MM , Year – YYYY” (Day – 27 ,Month – 07 , Year – 2022)
+//  #include <stdio.h>
+//  int main()
+//  {
+//      int date,mon,year;
+//      printf("Enter Date/Month/Year :: ");
+//      scanf("%d/%d/%d",&date,&mon,&year);
+//      printf("Date = %d/%d/%d",date,mon,year);
+//      return 0;
+//  }
+
+//Q11.  WAP to take time as an input in below given format and convert the time format and
+// display the result as given below.
+// User Input time format – “HH:MM”
+// Output format – “HH hour and MM Minute”
+// Example –
+// “11:25” converted to “11 Hour and 25 Minute”
+// #include <stdio.h>
+// int main()
+// {
+//     int minute,hour;
+//     printf("Enter Hours:Minutes :: ");
+//     scanf("%d : %d",&hour,&minute);
+//     printf("Time = %d Hour and %d Minute",hour,minute);
+//     return 0;
+// }
+
+//Q12. 12. Find output of below code:
+// #include <stdio.h>
+// int main()
+// {
+// int x = printf(“ineuron”);
+// printf("%d",x);
+// return 0;
+// }
+
+// Ans Output = Error
+
